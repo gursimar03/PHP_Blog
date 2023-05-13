7fa8c8242d4f28b0c2e1d9a2f493f7f690a00efa
gursimar03 <gursimar03@gmail.com>
2023-05-13 14:24:36 +0100

    user profile route and view added

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::resource('/blog', PostsController::class);
 Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+Route::get('/user', [\App\Http\Controllers\UserController::class, 'userProfile'])->name('user.profile');
+Route::post('/user/update-name', [\App\Http\Controllers\UserController::class, 'updateName'])->name('user.update-name');
+Route::post('/user/update-password', [\App\Http\Controllers\UserController::class, 'updatePassword'])->name('user.update-password');
+Route::delete('/user/delete-account', [\App\Http\Controllers\UserController::class, 'deleteAccount'])->name('user.delete-account');
+
+
+
